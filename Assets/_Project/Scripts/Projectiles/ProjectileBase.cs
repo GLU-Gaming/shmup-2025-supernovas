ac3dfa69b0bbf079452e1c8215099c8bf25d231c
Herekawa <sb.brunia@gmail.com>
2025-03-21 09:52:02 +0100

    added projectiles dealing damage

--- Assets/_Project/Scripts/Projectiles/ProjectileBase.cs
+++ Assets/_Project/Scripts/Projectiles/ProjectileBase.cs
@@ -1,15 +1,26 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileBase : MonoBehaviour
 {
     public Vector3 direction;
-    private float speed = 1;
-    private float lifespan = 10f;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float lifespan = 10f;
+    [SerializeField] private int piercing = 0; // 0 = infinite piercing, 1 = 1 piercing, 2,3,... = 2,3,... piercing
+    //private int totalHits;
     private float startTime;
+    private List<GameObject> targetsHit;
 
     private void Start()
     {
         startTime = Time.time;
+    }
+
+    void Awake()
+    {
+        targetsHit = new List<GameObject>();
+        //totalHits = 0;
     }
 
     private void FixedUpdate()
@@ -23,6 +34,17 @@ public class ProjectileBase : MonoBehaviour
         {
             Kill();
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        GameObject hit = other.gameObject;
+        if (targetsHit.Contains(hit)) { return; }
+        targetsHit.Add(hit);
+        hit.TryGetComponent<Health>(out Health hitHP);
+        if (!hitHP) { return; }
+        hitHP.TakeDamage(damage);
     }
 
     public void Kill()
