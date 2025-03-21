0ae313066519e5231eec2c4aac74977e69e77e82
Mennovos <mennodvos@gmail.com>
2025-03-21 09:59:16 +0100

    almost move up and down

--- Assets/_Project/Scripts/Enemies/EnemyBase.cs
+++ Assets/_Project/Scripts/Enemies/EnemyBase.cs
@@ -1,10 +1,40 @@
+using System.Threading;
 using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-public float HP;
-
-public void Death()
+    public float DownMovTim;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Movement();
+    }
+    private void Update()
+    {
+        DownMovTim += Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+        if (DownMovTim >= 10)
+        {
+            DownMovTim = 0;
+            Movement();
+        }
+    }
+    public void Movement()
+    {
+        if (DownMovTim < 2)
+        {
+          rb.AddForce(new Vector3(0,1,0));
+        }   
+        else 
+        {
+            rb.AddForce(new Vector3(0, -1, 0));
+        }
+    }
+    public void Death()
 {
 // add death effects, player score, possibly spawn a powerup
 
