429a83faad296393c3edbd1b72391980431714c1
Mennovos <mennodvos@gmail.com>
2025-03-24 09:54:36 +0100

    Projictile B Prefab

--- Assets/_Project/Scripts/Enemies/EnemyBase.cs
+++ Assets/_Project/Scripts/Enemies/EnemyBase.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    [SerializeField] GameObject Bullet;
+    [SerializeField] GameObject Enemy;
     public Vector3 pos;
+    private float BulletSpeed = 12.0f;
     private void Awake()
     {
         pos = transform.position;
     }
     public void Start()
     {
-
+        GameObject bulletObj = Instantiate(Bullet,Enemy.transform, Enemy.transform);
     }
     private void Update()
-    { 
-      transform.position = new Vector3(pos.x, pos.y + Mathf.Sin(Time.time)*4, 0);
+    {
+        transform.position = new Vector3(pos.x, pos.y + Mathf.Sin(Time.time) * 4, 0);
+         
     }
     public void Death()
-{
-// add death effects, player score, possibly spawn a powerup
+    {
+        // add death effects, player score, possibly spawn a powerup
 
-CleanUp();
-}
+        CleanUp();
+    }
 
-public void CleanUp()
-{
-    Destroy(gameObject);
-}
+    public void CleanUp()
+    {
+        Destroy(gameObject);
+    }
 
 }
