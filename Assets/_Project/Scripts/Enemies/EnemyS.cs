a7052b610708e6f453f948196b6850ffd9570400
Herekawa <sb.brunia@gmail.com>
2025-03-27 11:53:29 +0100

    added EnemyS anim

--- Assets/_Project/Scripts/Enemies/EnemyS.cs
+++ Assets/_Project/Scripts/Enemies/EnemyS.cs
@@ -1,19 +1,19 @@
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem.LowLevel;
 
-public class EnemyS : MonoBehaviour
+public class EnemyS : EnemyBase
 {
-    public Transform Circeltarget;
-    public Transform Hitpoint;
-    [SerializeField] private CreationService creationService;
-    [SerializeField] private float Speed = 5f;
-    [SerializeField] private float Radius = 5f;
-    [SerializeField] private float Angle = 5f;
+    [SerializeField] private Transform Firepoint;
+    private CreationService creationService;
 
-    float tiltAngle = 60.0f;
-    Quaternion target;
+
+    // dive anim parameters:
+    [SerializeField] float speed;
+    [SerializeField] float lenght;
+    [SerializeField] float depth;
+    [SerializeField] float rOffset;
+    [SerializeField] float rMult;
+
     void Start()
     {
         creationService = FindAnyObjectByType<CreationService>();
@@ -21,45 +21,24 @@ public class EnemyS : MonoBehaviour
     }
     private void FixedUpdate()
     {
-      
+
     }
+
     void Update()
     {
+        float scaledT = Time.time * speed;
+        float x = Mathf.Cos(scaledT);
+        float y = -Mathf.Abs(Mathf.Sin(scaledT));
+        float r = x * rMult + rOffset;
 
-        float x = Circeltarget.position.x + Mathf.Cos(Angle)* Radius;
-        float y = Circeltarget.position.y - Mathf.Abs(Mathf.Sin(Angle)) * Radius;  
-        float z = Circeltarget.position.z;
-
-        // Update the position of EnemyS
-        transform.position = new Vector3(x, y, z);
-
-        //increment the angle to move along the cricular path
-        Angle += Speed * Time.deltaTime;
-        if (transform.position.x >= 6)
-        {
-            // Rotate the cube by converting the angles into a quaternion.
-            Quaternion target = Quaternion.Euler(0, 0, 18);
-            // Dampen towards the target rotation
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
-        }
-
-        if (transform.position.y <= 2)
-        {
-            Quaternion target = Quaternion.Euler(0, 0, 0);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
-        }
-        if (transform.position.y >= 5)
-        {
-            Quaternion target = Quaternion.Euler(0, 0, -18);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
-        }
-
+        transform.SetPositionAndRotation(pos + new Vector3(x * lenght, y * depth, 0), Quaternion.AngleAxis(r, Vector3.forward));
     }
+
     IEnumerator MyCoroutine()
     {
         while (true)
         {
-            creationService.CreateProjectile(0, Hitpoint);
+            creationService.CreateProjectile(0, Firepoint);
             yield return new WaitForSeconds(2);
         }
     }
