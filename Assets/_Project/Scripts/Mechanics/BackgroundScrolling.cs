eb49f67cba364b6d635233c61c515d05a9a22533
Mennovos <mennodvos@gmail.com>
2025-04-02 09:46:58 +0200

    backgroundscrolling done

--- Assets/_Project/Scripts/Mechanics/BackgroundScrolling.cs
+++ Assets/_Project/Scripts/Mechanics/BackgroundScrolling.cs
@@ -1,16 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class BackgroundScrolling : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] public bool stop;
+    [SerializeField] public float speed;
+    [SerializeField] private Renderer m_RenderBuffer;
+    public void Start()
     {
-        
+        stop = false;
+        StartCoroutine(Stop());
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    IEnumerator Stop()
+    {
+        while (!stop)
+        {
+            m_RenderBuffer.material.mainTextureOffset -= new Vector2(speed * Time.deltaTime, 0);
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
