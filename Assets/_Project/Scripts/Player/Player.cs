02df0d1d0efa93b9cc0e1c773124d5315494a878
Herekawa <sb.brunia@gmail.com>
2025-04-03 09:56:55 +0200

    fixed player OOB

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -5,21 +5,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 1;
     [SerializeField] private Transform firePoint;
-    [SerializeField] private Vector2 Size;
+    [SerializeField] private Vector3 playerSize;
     private CreationService creationService;
     public float attackCooldown = 0.2f;
     private float lastAttack = 0;
-    Vector3 boundry;
     InputAction moveAction;
     InputAction attackAction;
+    Vector3 maxPos;
+    Vector3 minPos;
+
 
     void Awake()
     {
         creationService = FindAnyObjectByType<CreationService>();
-        boundry = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.transform.position.y));
         moveAction = InputSystem.actions.FindAction("Move");
         attackAction = InputSystem.actions.FindAction("Jump");
+
+        Vector3 boundry = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.transform.position.y));
+        Vector3 boundryOffset = Camera.main.transform.position;
+        Vector3 boundryZero = boundry - boundryOffset;
+        maxPos = boundryZero + boundryOffset - playerSize;
+        minPos = -boundryZero + boundryOffset + playerSize;
     }
+
 
     void FixedUpdate()
     {
@@ -29,8 +37,9 @@ public class Player : MonoBehaviour
         moveValue = 0.08f * speed * moveValue.normalized;
 
         Vector3 newPos = transform.position + new Vector3(moveValue.x, moveValue.y);
-        newPos.x = Mathf.Clamp(newPos.x, -boundry.x + Size.x * 0.5f, boundry.x - Size.x * 0.5f);
-        newPos.y = Mathf.Clamp(newPos.y, -boundry.y + Size.y * 0.5f, boundry.y - Size.y * 0.5f);
+
+        newPos.x = Mathf.Clamp(newPos.x, minPos.x, maxPos.x);
+        newPos.y = Mathf.Clamp(newPos.y, minPos.y, maxPos.y);
 
         transform.position = newPos;
     }
