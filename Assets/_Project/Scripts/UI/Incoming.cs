24764c9355d00a8fb54a1228b20b6592eb930058
Herekawa <sb.brunia@gmail.com>
2025-04-04 12:05:09 +0200

    tweaked positions of UI and enemyS spawn

--- Assets/_Project/Scripts/UI/Incoming.cs
+++ Assets/_Project/Scripts/UI/Incoming.cs
@@ -17,14 +17,12 @@ public class Incoming : MonoBehaviour
     {
         if (position.y > 5f) // i dont like this but it works 
         {
-            StartCoroutine(WarnAnim(new Vector3(0,6,0)));
+            StartCoroutine(WarnAnim(new Vector3(0,7,0)));
         }
         else
         {
             StartCoroutine(WarnAnim(new Vector3(0,-5,0)));
         }
-
-        //incomingUI.rectTransform.SetLocalPositionAndRotation(Camera.main.WorldToScreenPoint(position),Quaternion.identity);
 
         return new WaitForSeconds(warnTime);
     }
