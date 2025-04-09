d3db3962330cc602aa70a14986527e813f13672b
Mennovos <mennodvos@gmail.com>
2025-04-09 14:35:06 +0200

    ok

--- Assets/_Project/Scripts/Enemies/Boss.cs
+++ Assets/_Project/Scripts/Enemies/Boss.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -15,10 +16,8 @@ public class Boss : MonoBehaviour
     [SerializeField] public int amountEnemy;
     private Vector3 pos;
     public int Attack2;
-    bool AirAttack = false;
     void Start()
     {
-        AirAttack = false;
         projectileboss = FindAnyObjectByType<ProjectileBoss>();
         pos = transform.position;
         StartCoroutine(ShootingTime());
@@ -40,37 +39,39 @@ public class Boss : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, Pos2.transform.position, speed);
             HoldUp.SetActive(false);
             projectileboss.spinny = 0;
+            creationService.CreateEnemy(1, transform);
             StartCoroutine(StartspinnyAgian());
         }
-    }
-    IEnumerator AttackAir()
-    {
-        yield return new WaitForSeconds(15f);
-        Attack2 = 1;
-    }
-    IEnumerator StartspinnyAgian()
-    {
-        yield return new WaitForSeconds(5f);
-        Attack2 = 0;
-        HoldUp.SetActive(true);
-        projectileboss.Spinnystart();
-    }
-    IEnumerator ShootingTime()
-    {
-        while (true)
+
+        IEnumerator AttackAir()
         {
-            yield return new WaitForSeconds(3f);
-            creationService.CreateProjectile(0, firePoint);
-            if (projectileboss.spinny > 0)
+            yield return new WaitForSeconds(15f);
+            Attack2 = 1;
+        }
+        IEnumerator StartspinnyAgian()
+        {
+            yield return new WaitForSeconds(5f);
+            Attack2 = 0;
+            HoldUp.SetActive(true);
+            projectileboss.Spinnystart();
+        }
+        IEnumerator ShootingTime()
+        {
+            while (true)
             {
-                StartCoroutine(CreatingEnemy());
+                yield return new WaitForSeconds(3f);
+                creationService.CreateProjectile(0, firePoint);
+                if (projectileboss.spinny > 0)
+                {
+                    StartCoroutine(CreatingEnemy());
+                }
             }
         }
-    }
-    IEnumerator CreatingEnemy()
-    {
-        yield return new WaitForSeconds(2f);
-        Instantiate(Enemy, firePoint.transform.position, Quaternion.identity);
+        IEnumerator CreatingEnemy()
+        {
+            yield return new WaitForSeconds(2f);
+            Instantiate(Enemy, firePoint.transform.position, Quaternion.identity);
+        }
     }
 
 }
