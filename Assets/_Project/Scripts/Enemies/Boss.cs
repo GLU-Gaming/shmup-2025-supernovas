f3dbb8cb05f3d868ac605a9bed5799aa3535014c
Mennovos <mennodvos@gmail.com>
2025-04-10 14:17:36 +0200

    Update
    Works

--- Assets/_Project/Scripts/Enemies/Boss.cs
+++ Assets/_Project/Scripts/Enemies/Boss.cs
@@ -49,46 +49,46 @@ public class Boss : MonoBehaviour
             StartCoroutine(MoveScreen());
             StartCoroutine(StartspinnyAgian());
         }
-        IEnumerator MoveScreen()
-        {
-            yield return new WaitForSeconds(5f);
-            Pos2.transform.position = new Vector3(-8, -3, 0);
-        }
-        IEnumerator AttackAir()
-        {
-            yield return new WaitForSeconds(15f);
-            Attack2 = 1;
-        }
-        IEnumerator StartspinnyAgian()
-        {
-            yield return new WaitForSeconds(15f);
-            Attack2 = 0;
-            HoldUp.SetActive(true);
-            projectileboss.Spinnystart();
-        }
-//        IEnumerator ShootingTime()
-//        {
-//            while (true)
-//            {
-//                yield return new WaitForSeconds(3f);
-//                creationService.CreateProjectile(0, firePoint);
-//                if (projectileboss.spinny > 0)
-//                {
-//                    StartCoroutine(CreatingEnemy());
-//                }
-//            }
-//        }
-//        IEnumerator CreatingEnemy()
-//        {
-//            yield return new WaitForSeconds(2f);
-//            Instantiate(Enemy, firePoint.transform.position, Quaternion.identity);
-//        }
+        //        IEnumerator ShootingTime()
+        //        {
+        //            while (true)
+        //            {
+        //                yield return new WaitForSeconds(3f);
+        //                creationService.CreateProjectile(0, firePoint);
+        //                if (projectileboss.spinny > 0)
+        //                {
+        //                    StartCoroutine(CreatingEnemy());
+        //                }
+        //            }
+        //        }
+        //        IEnumerator CreatingEnemy()
+        //        {
+        //            yield return new WaitForSeconds(2f);
+        //            Instantiate(Enemy, firePoint.transform.position, Quaternion.identity);
+        //        }
     }
-           public void Victory()
-         {
-           Win.SetActive(true);
-           Destroy(gameObject);
-           Destroy(Player);
-           HoldUp.SetActive(false);
-         }
+    IEnumerator MoveScreen()
+    {
+        yield return new WaitForSeconds(5f);
+        Pos2.transform.position = new Vector3(-8, -3, 0);
+    }
+    IEnumerator AttackAir()
+    {
+        yield return new WaitForSeconds(15f);
+        Attack2 = 1;
+    }
+    IEnumerator StartspinnyAgian()
+    {
+        yield return new WaitForSeconds(15f);
+        Attack2 = 0;
+        HoldUp.SetActive(true);
+        projectileboss.Spinnystart();
+    }
+    public void Victory()
+    {
+        Win.SetActive(true);
+        Destroy(gameObject);
+        Destroy(Player);
+        HoldUp.SetActive(false);
+    }
 }
